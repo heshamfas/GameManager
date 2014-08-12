500ea9b3eceb4273d2fae3e8d6b7c79324815f7b
HeshamFas <HeshamFas@live.com>
2014-08-11 21:16:35 -0700

    Changing GameObjectList to be global in the Application Object

---
+++ app/src/main/java/com/games/hesham/gamemanager/fragment/GeneralDialogFragment.java
@@ -0,0 +1,31 @@
+package com.games.hesham.gamemanager.fragment;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by Hesham on 8/11/2014.
+ */
+public class GeneralDialogFragment extends DialogFragment {
+    Context mContext;
+    public GeneralDialogFragment() {
+        mContext = getActivity();
+    }
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Bundle bundle = this.getArguments();
+        String title = bundle.getString("title");
+        String message = bundle.getString("message");
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+        alertDialogBuilder.setTitle(title);
+        alertDialogBuilder.setMessage(message);
+        //null should be your on click listener
+        alertDialogBuilder.setPositiveButton("OK", null);
+        return alertDialogBuilder.create();
+    }
+}
